d281203842d1acef16815c17fff7d2a919d1d84e
Abdenasser <nasser.elidrissi065@gmail.com>
2024-03-27 02:55:46 +0000

    changes

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"log"
 )
 
+const NoWinner = "none"
+const BoardSize = 9
+const OutputLines = 4
+
 var colors = map[string][2]string{
 	"blue":  {"\x1b[34m", "\x1b[0m"},
 	"cyan":  {"\x1b[36m", "\x1b[0m"},
@@ -20,26 +24,25 @@ type Player struct {
 	Value string
 }
 
-var player1 = Player{colorize("[Player x]", "blue"), colorize("x", "blue")}
-var player2 = Player{colorize("[Player o]", "cyan"), colorize("o", "cyan")}
+var playerX = Player{colorize("[Player x]", "blue"), colorize("x", "blue")}
+var playerO = Player{colorize("[Player o]", "cyan"), colorize("o", "cyan")}
 
 func getPlayer(t int) Player {
 	if t%2 == 0 {
-		return player1
+		return playerX
 	} else {
-		return player2
+		return playerO
 	}
 }
 
 func printBoard(b map[int]string) {
-	for i := 0; i < 9; i += 3 {
+	for i := 0; i < BoardSize; i += 3 {
 		fmt.Printf("%v | %v | %v\n", colorize(b[i], "black"), colorize(b[i+1], "black"), colorize(b[i+2], "black"))
 	}
 }
 
 func clearOutput() {
-	// assuming we print 4 lines at each iteration
-	for i := 0; i < 5; i++ {
+	for i := 0; i <= OutputLines; i++ {
 		fmt.Printf("\033[1A\033[K")
 	}
 }
@@ -58,7 +61,7 @@ func isPosChecked(pos int, b map[int]string) bool {
 	return false
 }
 
-func playTurn(b map[int]string, t int) (map[int]string, int, error) {
+func playTurn(b map[int]string, t int) (map[int]string, int) {
 	var pos int
 	player := getPlayer(t)
 
@@ -66,17 +69,16 @@ func playTurn(b map[int]string, t int) (map[int]string, int, error) {
 	_, err := fmt.Scanln(&pos)
 
 	if err != nil {
-		return b, t, err
+		log.Fatal("Not a valid position ", err)
 	}
 
 	if isPosChecked(pos, b) || isPosOutOfRange(pos) {
 		// just return whatever
-		return b, t, nil
+		return b, t
 	}
 
 	b[pos] = player.Value
-	t += 1
-	return b, t, nil
+	return b, t + 1
 }
 
 func checkWinner(b map[int]string) string {
@@ -93,30 +95,29 @@ func checkWinner(b map[int]string) string {
 		}
 	}
 
-	return "none"
+	return NoWinner
+}
+
+func initBoard() map[int]string {
+	board := make(map[int]string)
+	for i := 0; i < BoardSize; i++ {
+		board[i] = colorize(fmt.Sprintf("%d", i), "black")
+	}
+	return board
 }
 
 func main() {
-	board := make(map[int]string)
-	for i := 0; i < 9; i++ {
-		board[i] = colorize(fmt.Sprintf("%d", i), "black")
-	}
+	board := initBoard()
 	turn := 0
-	for turn < 9 {
+	for turn < BoardSize {
 		printBoard(board)
-		var err error
-		board, turn, err = playTurn(board, turn)
-		if err != nil {
-			log.Fatal("Not a valid position ", err)
-		}
+		board, turn = playTurn(board, turn)
 		winner := checkWinner(board)
-		if winner != "none" {
-			clearOutput()
-			printBoard(board)
-			fmt.Printf("%v wins\n", winner)
-			return
-		}
 		clearOutput()
+		if winner != NoWinner {
+			fmt.Printf("%v wins\n", winner)
+			break
+		}
 	}
 
 }
